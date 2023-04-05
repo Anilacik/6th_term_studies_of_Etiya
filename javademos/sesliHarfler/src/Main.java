73f5b29d0c113aa644686ce1539a38d72ee3a064
Anıl AÇIK <anil_acik_1997@hotmail.com>
2023-04-05 19:32:41 +0300

    commit

---
+++ javademos/sesliHarfler/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        char harf = 'E';
+
+        switch (harf){
+            case 'A','I','O','U':
+        System.out.println("kalın sesli harf");
+        break;
+            default:
+                System.out.println("ince sesli harf");
+
+        }
+    }
+}
